ccb194dc07cfb650ddf5d65c74de83358831ec07
Philipp Bosch <hello@pb.io>
2012-08-21 17:21:19 +0200

    Bump version to 0.3.0

--- pushscreen/__init__.py
+++ pushscreen/__init__.py
@@ -10,7 +10,7 @@ pushscreen
 """
 
 __title__ = 'PushScreen'
-__version__ = '0.2.4'
+__version__ = '0.3.0'
 __author__ = 'Philipp Bosch'
 __license__ = 'ISC'
 __copyright__ = 'Copyright 2012 Philipp Bosch'
